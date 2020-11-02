fb248114d9dd4caa46e43f2908e78aff9ec6e036
Warton Barros <58346487+wartonbarros@users.noreply.github.com>
2020-11-02 18:26:18 -0300

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -9,12 +9,12 @@
     });
 
 
-    // $('#indice, a[href^="#"]').on('click', function(e) {
-    // 	e.preventDefault();
-    // 	var id = $(this).attr('href'),
-    // 			targetOffset = $(id).offset().top;
-    //
-    // 	$('html, body').animate({
-    // 		scrollTop: targetOffset - 10
-    // 	}, 2000);
-    // });
+    $('#indice, a[href^="#"]').on('click', function(e) {
+    	e.preventDefault();
+    	var id = $(this).attr('href'),
+    			targetOffset = $(id).offset().top;
+    
+    	$('html, body').animate({
+    		scrollTop: targetOffset - 10
+    	}, 2000);
+    });
